5bd1322108e5577d9b63e7c153327a23b682bac3
melissasedgwick <melissa.sedgwick@yahoo.co.uk>
2018-09-26 14:41:14 +0100

    Added one_year_passes and count_oranges method to orange_tree.rb

--- orange_tree.rb
+++ orange_tree.rb
@@ -6,5 +6,18 @@ class OrangeTree
     @number_of_oranges = 0
   end
 
+  def one_year_passes
+    @years_old += 1
+    @height += 2
+  end
 
+  def count_oranges
+    if @number_of_oranges = 0
+      puts "Unfortunately, there are no oranges on your tree!"
+    elsif @number_of_oranges = 1
+      puts "You have 1 orange on your tree!"
+    else
+      puts "There are #{@number_of_oranges} on your tree!"
+    end
+  end
 end
